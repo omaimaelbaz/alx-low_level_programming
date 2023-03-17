79a7443d3f2bfc233a94070731980f1b5748e610
omaimaelbaz <oumcode01@gmail.com>
2023-03-17 03:02:06 +0100

    betty run

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- *  * main - display the sentence
- *   *
- *    * Return: always 0 (success)
+ *  * main - Entry point
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *    * Return: 0
  *     */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
