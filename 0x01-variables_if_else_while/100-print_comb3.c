ea27de427d1a2387323d58ea08b6796da6a49772
omaimaelbaz <elbazouma70@gmail.com>
2023-03-17 19:55:37 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - a program that displays the possible combs of two digits
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '8'; i++)
+	{
+		for (j = i + 1; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '8' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
