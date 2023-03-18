0379a6045fa141b0d6ccde8d3dbed503b5a0b2fd
omaimaelbaz <elbazouma70@gmail.com>
2023-03-17 19:57:20 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - a program that displays the possible combs of two digits
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i <= '7'; i++)
+	{
+		for (j = i + 1; j <= '8'; j++)
+		{
+			for (k = j + 1; k <= '9'; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != '7' || j != '8' || k != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
