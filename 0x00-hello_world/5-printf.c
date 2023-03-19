21f5b12333d4b439d097fa0909ed0f12557270af
omaimaelbaz <elbazouma70@gmail.com>
2023-03-19 10:39:13 -0700

    5555

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- *  * main - Print a string
- *   * Description: use printf
- *    * Return: 0
- *     */
-
+ * main - display the sentence
+ *
+ * Return: 0 means exit with success
+ */
 int main(void)
 {
-		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
