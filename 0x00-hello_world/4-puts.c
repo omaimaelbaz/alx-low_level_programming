d21eba6fb73671090eff40bf530ad19d516218ce
omaimaelbaz <elbazouma70@gmail.com>
2023-03-19 10:37:06 -0700

    444

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - display the sentence
  *
  * Return: always 0 (success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
