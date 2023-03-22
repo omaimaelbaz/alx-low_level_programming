8d0c2b8e7063a688e9d90ff8025c661e9cecb10c
omaimaelbaz <elbazouma70@gmail.com>
2023-03-22 14:58:26 -0700

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_to_98 - print numbers to 98
+* @n: parametre
+*/
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+		printf("%d\n", n);
+	}
+}
