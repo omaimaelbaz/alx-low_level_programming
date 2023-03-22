3bbd3d4f4eb8e7de02ae8059a8e1dc3576b69b0f
omaimaelbaz <elbazouma70@gmail.com>
2023-03-22 04:31:23 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - get print_last_digit
+ * @n: the parametre
+ * Return: n if n is positive or -n if n is negative
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
