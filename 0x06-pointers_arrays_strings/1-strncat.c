84a6d87f38a2749e5ce0c25ba430fb04c42f3bc9
omaimaelbaz <oumcode01@gmail.com>
2023-03-30 10:45:37 -0700

    😂

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - copy from src to dst
+ * @dest: parametre
+ * @src: parametre
+ * @n: parametre
+ * Return: string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
