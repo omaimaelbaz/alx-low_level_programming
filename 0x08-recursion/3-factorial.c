af68b9da88729a89052173f7daf1d323601de0ef
omaimaelbaz <oumcode01@gmail.com>
2023-04-06 09:28:00 -0700

    tsk 3ヾ(•ω•`)o

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,11 +8,10 @@
  */
 int factorial(int n)
 {
-    if (n < 0)
-    return -1;
-    else if  (n <= 1)
-    return 1 ;
-    else
-    return n * factorial(n-1) ;
-
+	if (n < 0)
+		return (-1);
+	else if (n <= 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
 }
